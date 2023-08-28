cce0c9c5187767b5dde4e5a484f366e190e5e025
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-08-28 10:08:44 +0700

    home

--- pages/index.vue
+++ pages/index.vue
@@ -51,64 +51,67 @@
 
     <section class="mt-[100px] w-full">
       <div class="max-w-7xl mx-auto">
-        <div class="">
+        <div class="mb-5">
           <h1 class="text-3xl font-semibold">Units</h1>
         </div>
 
-        <div
-          class="flex flex-col md:flex-row md:flex-wrap gap-5 justify-center"
-        >
-          <!-- card -->
+        <!-- card -->
+        <nuxt-link to="/product" class="grid grid-cols-3 gap-5 justify-between">
           <div
             v-for="data in 9"
             :key="data"
-            class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            class="max-w-sm p-6 bg-white border mb-5 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
-            <svg
-              class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"
-              />
-            </svg>
-            <a href="#">
-              <h5
-                class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"
-              >
-                Need a help in Claim?
-              </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
-              Go to this step by step guideline process on how to certify for
-              your weekly benefits:
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center text-blue-600 hover:underline"
-            >
-              See our guideline
-              <svg
-                class="w-3 h-3 ml-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
-                />
-              </svg>
-            </a>
+            <img
+              src="../assets/images/cat-logo.png"
+              alt="logo"
+              class="w-48 mx-auto"
+            />
           </div>
-          <!-- end card -->
+        </nuxt-link>
+        <!-- end card -->
+      </div>
+    </section>
+
+    <section class="mt-[100px] w-full">
+      <div class="max-w-7xl mx-auto">
+        <div class="mb-5">
+          <h1 class="text-3xl font-semibold">Featured Products</h1>
+        </div>
+        <div class="grid grid-cols-3 gap-5 justify-between">
+          <div
+            v-for="data in 3"
+            :key="data"
+            class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+          >
+            <a href="#">
+              <img
+                class="p-8 rounded-t-lg"
+                src="../assets/images/feat1.jpg"
+                alt="product image"
+              />
+            </a>
+            <div class="px-5 pb-5">
+              <a href="#">
+                <h5
+                  class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+                >
+                  Collar Roof Guard Komatsu
+                </h5>
+              </a>
+              <div class="flex items-center mt-2.5 mb-5">Stock : 1</div>
+              <div class="flex items-center justify-between">
+                <span class="text-3xl font-bold text-gray-900 dark:text-white"
+                  >$599</span
+                >
+                <a
+                  href="#"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >Tanya Produk ini</a
+                >
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
